10bf13b7dff0e3af6ab06ee285c8a31795c9effc
Julien GOTTELAND <julien.gotteland@gmail.com>
2022-12-21 23:06:10 +0100

    Clean Day 16

--- day16/main.go
+++ day16/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func main() {
 	cave := ParseCave("input.txt", 30)
 
-	log.Printf("ReleaseMax: %v", cave.ReleaseMax()) // 1 580
+	log.Printf("ReleaseMax: %v", cave.ReleaseMax(Single())) // 1 580
 }
--- day16/types.go
+++ day16/types.go
@@ -40,24 +40,93 @@ func ParseValve(s string) Valve {
 	}
 }
 
-type State struct {
+type Cave struct {
+	Minutes           int
+	Valves            map[string]Valve
+	maxReleaseByState map[StateKey]int
+}
+
+func (c Cave) Pressure(s State) int {
+	result := 0
+
+	for _, name := range s.OpenValves {
+		result += c.Valves[name].FlowRate
+	}
+
+	return result
+}
+
+func (c Cave) doReleaseMax(s State) int {
+	if s.Iteration == c.Minutes {
+		return 0
+	}
+
+	result := 0
+	valve := c.Valves[s.Position]
+
+	if valve.FlowRate != 0 && !s.IsOpen(s.Position) {
+		result = c.ReleaseMax(s.Open())
+	}
+
+	for _, position := range valve.Tunnels {
+		result = aoc2022.Max(result, c.ReleaseMax(s.Move(position)))
+	}
+
+	return result + c.Pressure(s)
+}
+
+func (c Cave) ReleaseMax(s State) int {
+	key := s.StateKey()
+	result, exist := c.maxReleaseByState[key]
+
+	if exist {
+		return result
+	}
+
+	result = c.doReleaseMax(s)
+
+	c.maxReleaseByState[key] = result
+
+	return result
+}
+
+func (c Cave) String(s State) string {
+	sb := strings.Builder{}
+
+	sb.WriteString(fmt.Sprintf("== Iteration %d ==\n", s.Iteration))
+
+	switch len(s.OpenValves) {
+	case 0:
+		sb.WriteString("No valves are open.\n")
+	case 1:
+		sb.WriteString(fmt.Sprintf("Valve %v is open, releasing %v pressure.\n", s.OpenValves[0], c.Pressure(s)))
+	default:
+		sb.WriteString(fmt.Sprintf("Valves %v are open, releasing %v pressure.\n", s.OpenValves, c.Pressure(s)))
+	}
+
+	sb.WriteString(fmt.Sprintf("Position: %v\n\n", s.Position))
+
+	return sb.String()
+}
+
+type StateKey struct {
 	Minute     int
 	OpenValves string
 	Position   string
 }
 
-var cache = make(map[State]int)
-
-type Cave struct {
+type State struct {
 	Iteration  int
-	Minutes    int
 	OpenValves []string
 	Position   string
-	Valves     map[string]Valve
 }
 
-func (c Cave) IsOpen(valve string) bool {
-	for _, name := range c.OpenValves {
+func Single() State {
+	return State{Position: "AA"}
+}
+
+func (s State) IsOpen(valve string) bool {
+	for _, name := range s.OpenValves {
 		if name == valve {
 			return true
 		}
@@ -66,130 +135,39 @@ func (c Cave) IsOpen(valve string) bool {
 	return false
 }
 
-func (c Cave) Names() []string {
-	result := make([]string, 0)
-
-	for name := range c.Valves {
-		result = append(result, name)
-	}
-
-	sort.Strings(result)
-
-	return result
-}
-
-func (c Cave) Move(position string) Cave {
-	return Cave{
-		Iteration:  c.Iteration + 1,
-		Minutes:    c.Minutes,
-		OpenValves: c.OpenValves,
-		Position:   position,
-		Valves:     c.Valves,
-	}
-}
-
-func (c Cave) Open() Cave {
-	openValves := c.OpenValves
-
-	openValves = append(openValves, c.Position)
-
-	return Cave{
-		Iteration:  c.Iteration + 1,
-		Minutes:    c.Minutes,
-		OpenValves: openValves,
-		Position:   c.Position,
-		Valves:     c.Valves,
-	}
-}
-
-func (c Cave) Pressure() int {
-	result := 0
-
-	for _, name := range c.OpenValves {
-		result += c.Valves[name].FlowRate
-	}
-
-	return result
-}
-
-func (c Cave) doReleaseMax() int {
-	if c.Iteration == c.Minutes {
-		return 0
-	}
-
-	result := 0
-	valve := c.Valves[c.Position]
-
-	if valve.FlowRate != 0 && !c.IsOpen(c.Position) {
-		result = c.Open().ReleaseMax()
-	}
-
-	for _, position := range valve.Tunnels {
-		result = aoc2022.Max(result, c.Move(position).ReleaseMax())
-	}
-
-	return result + c.Pressure()
-}
-
-func (c Cave) ReleaseMax() int {
-	state := c.State()
-	result, exist := cache[state]
-
-	if exist {
-		return result
-	}
-
-	result = c.doReleaseMax()
-
-	cache[state] = result
-
-	return result
-}
-
-func (c Cave) State() State {
+func (s State) Move(position string) State {
 	return State{
-		Minute:     c.Iteration,
-		OpenValves: strings.Join(c.OpenValves, ","),
-		Position:   c.Position,
+		Iteration:  s.Iteration + 1,
+		OpenValves: s.OpenValves,
+		Position:   position,
 	}
 }
 
-func (c Cave) Status() string {
-	sb := strings.Builder{}
+func (s State) Open() State {
+	openValves := s.OpenValves
 
-	for _, name := range c.Names() {
-		sb.WriteString(c.Valves[name].String())
-		sb.WriteString("\n")
+	openValves = append(openValves, s.Position)
+
+	return State{
+		Iteration:  s.Iteration + 1,
+		OpenValves: openValves,
+		Position:   s.Position,
 	}
-
-	return sb.String()
 }
 
-func (c Cave) String() string {
-	sb := strings.Builder{}
-
-	sb.WriteString(fmt.Sprintf("== Iteration %d ==\n", c.Iteration))
-
-	switch len(c.OpenValves) {
-	case 0:
-		sb.WriteString("No valves are open.\n")
-	case 1:
-		sb.WriteString(fmt.Sprintf("Valve %v is open, releasing %v pressure.\n", c.OpenValves[0], c.Pressure()))
-	default:
-		sb.WriteString(fmt.Sprintf("Valves %v are open, releasing %v pressure.\n", c.OpenValves, c.Pressure()))
+func (s State) StateKey() StateKey {
+	return StateKey{
+		Minute:     s.Iteration,
+		OpenValves: strings.Join(s.OpenValves, ","),
+		Position:   s.Position,
 	}
-
-	sb.WriteString(fmt.Sprintf("Position: %v\n\n", c.Position))
-
-	return sb.String()
 }
 
 func ParseCave(fileName string, minutes int) Cave {
 	result := Cave{
-		Minutes:    minutes,
-		OpenValves: make([]string, 0),
-		Position:   "AA",
-		Valves:     make(map[string]Valve),
+		Minutes:           minutes,
+		Valves:            make(map[string]Valve),
+		maxReleaseByState: make(map[StateKey]int),
 	}
 
 	lr := aoc2022.NewLineReader(fileName)
